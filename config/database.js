b147b9c5773d3d251b8c1277442ef14df4727afd
AJmal <96195451+Brahm03@users.noreply.github.com>
2022-09-27 18:39:15 +0500

    postgres

--- config/database.js
+++ config/database.js
@@ -1,11 +1,18 @@
-const path = require('path');
-
+// strapi-api/config/database.js
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('DATABASE_HOST', 'ec2-54-91-223-99.compute-1.amazonaws.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'd5o7c8l58po0jv'),
+      user: env('DATABASE_USERNAME', 'uykrfnhsxkglvi'),
+      password: env('DATABASE_PASSWORD', '9c41aba04f43f12b0986de453d3b428c5b40c2c38f736531bb68d413112eee8b'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
