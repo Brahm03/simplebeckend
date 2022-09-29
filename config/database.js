e1c2a4cb2d526122bc1c50abb15414abef683e94
AJmal <96195451+Brahm03@users.noreply.github.com>
2022-09-29 10:15:33 +0500

    fixed

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'ec2-54-91-223-99.compute-1.amazonaws.com'),
+      host: env('DATABASE_HOST', 'ec2-3-220-207-90.compute-1.amazonaws.com'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'd5o7c8l58po0jv'),
-      user: env('DATABASE_USERNAME', 'uykrfnhsxkglvi'),
-      password: env('DATABASE_PASSWORD', '9c41aba04f43f12b0986de453d3b428c5b40c2c38f736531bb68d413112eee8b'),
+      database: env('DATABASE_NAME', 'd548tc8qv9ci2r'),
+      user: env('DATABASE_USERNAME', 'wezbsdlncunegh'),
+      password: env('DATABASE_PASSWORD', 'f09c805be7d2517c200b79afb3094b46abd92571c6ecb0090be09bb5c29de4a9'),
       schema: env('DATABASE_SCHEMA', 'public'), // Not required
       ssl: {
         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
